ec17275aebdb686f62da706f47bbb24800a75b7e
shinagupta <132837582+shinagupta@users.noreply.github.com>
2025-01-28 11:42:08 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "public_subnet_cidr" {
+  value = aws_subnet.public_subnet.cidr_block
+}
+
+output "private_subnet_cidr" {
+  value = aws_subnet.private_subnet.cidr_block
+}
