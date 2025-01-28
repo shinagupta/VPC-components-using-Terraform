3cfa73c0e1fbfd1449aadf0564ba316ba9d323c1
shinagupta <132837582+shinagupta@users.noreply.github.com>
2025-01-28 11:39:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,91 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_eip" "my_eip" {
+  tags = {
+    Name = "MyEIP"
+  }
+}
+
+resource "aws_nat_gateway" "my_nat_gw" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id      = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "my-nat-gw"
+  }
+}
+
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id          = aws_nat_gateway.my_nat_gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
